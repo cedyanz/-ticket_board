ad038de73564e854e6f4dde73b961cb079f0df97
cedyanz <cedyanz@gmail.com>
2025-03-28 02:54:35 +0000

    Add login service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DepartmentComponent } from './pages/department/department.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full',
+    },
+    {
+        path:'login',
+        component:LoginComponent,
+    },
+    {
+        path:'',
+        component:LoginComponent,
+        children :[
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            },
+            {
+                path:'department',
+                component:DepartmentComponent
+            }
+        ]
+    },
+];
